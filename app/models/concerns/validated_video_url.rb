92987ed8504b8303314ce52d1a199b19bc9d1eab
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2020-08-28 10:29:16 -0600

    Allow Vimeo URLs for pitch contest entries too

--- app/models/concerns/validated_video_url.rb
+++ app/models/concerns/validated_video_url.rb
@@ -2,13 +2,18 @@ module ValidatedVideoUrl
   extend ActiveSupport::Concern
 
   YOUTUBE_REGEX = %r{\A(?:http://|https://)?youtu.be/(\S+)\z}
+  VIMEO_REGEX = %r{\A(?:http://|https://)?vimeo.com/(\d+)\z}
 
   included do
     validates :video_url,
-      format: {with: YOUTUBE_REGEX, allow_blank: true}
+      format: {with: Regexp.union(YOUTUBE_REGEX, VIMEO_REGEX), allow_blank: true}
   end
 
   def embed_video_url(extra_params = {modestbranding: 1, showinfo: 0})
-    "https://www.youtube.com/embed/#{YOUTUBE_REGEX.match(video_url)[1]}?#{extra_params.to_query}"
+    if (result = YOUTUBE_REGEX.match(video_url))
+      "https://www.youtube.com/embed/#{result[1]}?#{extra_params.to_query}"
+    elsif (result = VIMEO_REGEX.match(video_url))
+      "https://player.vimeo.com/video/#{result[1]}"
+    end
   end
 end
