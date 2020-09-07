501ed561f7ca00e680bc30e423c01690cee09a10
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2020-09-07 10:45:36 -0600

    Allow oauth services to refresh their tokens
    Use database locks to ensure that we don’t try to refresh multiple times simultaneously.

--- app/models/oauth_service.rb
+++ app/models/oauth_service.rb
@@ -13,4 +13,37 @@ class OauthService < ApplicationRecord
     record.token_expires_at = Time.at(auth_hash["credentials"]["expires_at"]).to_datetime
     record.save
   end
+
+  def refresh_if_needed!
+    if token_expires_at.past?
+      with_lock "ACCESS EXCLUSIVE" do
+        # Avoid thundering herds - `with_lock` will reload the record, which means
+        # it will suddenly be non-expired if another process has refreshed it while
+        # we were waiting on the lock
+        return unless token_expires_at.past?
+        oauth = if provider.youtube?
+          OmniAuth::Strategies::GoogleOauth2.new(
+            nil, # App - nil is fine since we're outside of Rack
+            ENV["GOOGLE_CLIENT_ID"],
+            ENV["GOOGLE_CLIENT_SECRET"],
+            "profile,youtube",
+            name: "youtube"
+          )
+        end
+        token = OAuth2::AccessToken.new(
+          oauth.client,
+          token,
+          {refresh_token: refresh_token}
+        )
+        new_token = token.refresh!
+        if new_token.present?
+          update!(
+            token: new_token.token,
+            refresh_token: new_token.refresh_token,
+            token_expires_at: Time.at(new_token.expires_at).to_datetime
+          )
+        end
+      end
+    end
+  end
 end
