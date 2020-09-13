8898c4f74ca44b1d5bf9c4bb443a6aacc366f0bb
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2020-09-12 21:08:53 -0600

    Silence warnings when monkey patching Zoom client

--- config/initializers/zoom.rb
+++ config/initializers/zoom.rb
@@ -1,9 +1,11 @@
 # Override this to get access to newer settings per https://github.com/untitledstartup/zoom_rb/blob/5954cadb513f866c1a3eb1fedd47d9a0f25b2ca4/lib/zoom/actions/webinar.rb
-Zoom::Actions::Webinar::SETTINGS_KEYS = %i[panelists_video practice_session hd_video approval_type
-  registration_type audio auto_recording enforce_login
-  enforce_login_domains alternative_hosts close_registration
-  show_share_button allow_multiple_devices registrants_confirmation_email
-  meeting_authentication host_video post_webinar_survey survey_url].freeze
+Kernel.silence_warnings do
+  Zoom::Actions::Webinar::SETTINGS_KEYS = %i[panelists_video practice_session hd_video approval_type
+    registration_type audio auto_recording enforce_login
+    enforce_login_domains alternative_hosts close_registration
+    show_share_button allow_multiple_devices registrants_confirmation_email
+    meeting_authentication host_video post_webinar_survey survey_url].freeze
+end
 
 # Monkeypatch to fix Zoom::Error ({:base=>"Request Body should be a valid JSON object."})
 # Per https://github.com/hintmedia/zoom_rb/issues/25
